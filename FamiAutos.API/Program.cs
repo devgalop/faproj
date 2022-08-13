2040869702e200deb816abcd570a0cab24b57202
devgalop <devgalop@gmail.com>
2022-08-12 20:39:35 -0500

    Add CORS and Log

--- FamiAutos.API/Program.cs
+++ FamiAutos.API/Program.cs
@@ -2,32 +2,62 @@ using FamiAutos.API.Data;
 using FamiAutos.API.Data.Repositories;
 using FamiAutos.API.Helpers;
 using Microsoft.EntityFrameworkCore;
+using NLog;
+using NLog.Web;
 
-var builder = WebApplication.CreateBuilder(args);
+var logger = NLog.LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+logger.Debug("init main");
 
-builder.Services.AddControllers();
-builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-builder.Services.AddDbContext<DataContext>(options =>
+try
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DBFA"));
-});
-builder.Services.AddScoped<IAccountingRepository,AccountingRepository>()
-                .AddScoped<IMapperHelper,MapperHelper>();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+    var builder = WebApplication.CreateBuilder(args);
 
-var app = builder.Build();
+    builder.Services.AddControllers();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    // NLog: Setup NLog for Dependency injection
+    builder.Logging.ClearProviders();
+    builder.Host.UseNLog();
+
+    builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+    builder.Services.AddDbContext<DataContext>(options =>
+    {
+        options.UseSqlServer(builder.Configuration.GetConnectionString("DBFA"));
+    });
+    builder.Services.AddScoped<IAccountingRepository,AccountingRepository>()
+                    .AddScoped<IMapperHelper,MapperHelper>();
+    builder.Services.AddCors(options =>
+    {
+        options.AddPolicy(name: "MyCors", builder =>
+        {
+            builder.SetIsOriginAllowed(origin => new Uri(origin).Host == "localhost") 
+            .AllowAnyHeader().AllowAnyMethod();
+        });
+    }
+    );
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    var app = builder.Build();
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+    app.UseCors("MyCors");
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception ex){
+    logger.Error(ex, "Stopped program because of exception");
+    throw;
+}
+finally
+{
+    NLog.LogManager.Shutdown();
+}
